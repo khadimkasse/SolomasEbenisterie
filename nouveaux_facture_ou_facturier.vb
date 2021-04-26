59be3164c12aa701efced2beef7bda564d666609
bamba_kasse <khadimkc@gmx.com>
2021-04-26 02:52:57 +0200

    updated code so that the active sheet is the base of the new invoice

--- nouveaux_facture_ou_facturier.vb
+++ nouveaux_facture_ou_facturier.vb
@@ -1,19 +1,35 @@
 Option Explicit
 
 Sub createNewInvoice()
-    'We save the number of the invoice if it was not yet donein case the active sheet isn't the template
-    If ActiveSheet.Name <> "template" Then
+    Dim sheetName As String
+    sheetName = Application.InputBox("Saisissez le nom de la facture", Type:=2)
+    'The selected sheet is the base for the new invoice we create
+    'If the selected sheet was template, we just copy it as it is ready. Else, we need to modify it a little bit
+    If ActiveSheet.Name = "template" Then
+        'We copy the sheet template to a new location with the name input by the user
+        ActiveSheet.Copy after:=Sheets("informations enregistrées")
+        ActiveSheet.Name = sheetName
+    Else
+        'We save the number of the invoice if it was not yet done in case the active sheet isn't the template
         Range("invoiceNumber").Copy
         Range("invoiceNumber").PasteSpecial _
         Paste:=xlPasteValues
         Application.CutCopyMode = False
+        'We copy the current sheet to a new location with the name input by the user
+        ActiveSheet.Copy after:=ActiveSheet
+        ActiveSheet.Name = sheetName
+        'We empty the content of the table Appel de fond
+        'First we delete the lines above the 2nd one
+        Dim startRange As Integer : startRange = Range("MontantsTVAAppeles").Row  
+        Dim endRange As Integer : endRange = Range("MontantsTVAAppeles").Row + Range("MontantsTVAAppeles").Rows.Count - 1
+        Range("A" & startRange + 4 & _ 
+        ":S" & endRange).Delete Shift:=xlUp
+        'Then we delete the content of the percentage and replace the ref. devis by XXX
+        Range("F" & startRange).Value = 0
+        Range("G" & startRange).Value = "XXX"
+        Range("F" & startRange + 2).Value = 0
+        Range("G" & startRange + 2).Value = "XXX"
     End If
-
-    'We copy the sheet template to a new location with a named input by the user
-    Dim sheetName As String
-    sheetName = Application.InputBox("Saisissez le nom de la facture", Type:=2)
-    Sheets("template").Copy before:=Sheets("informations enregistrées")
-    ActiveSheet.Name = sheetName
 End Sub
 
 Sub createNewFacturier()
